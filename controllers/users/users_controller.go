9d0b21116b03a955e3dc97e8ac7e92ccd8dc6fca
damilare <damilare.durojaye@gmail.com>
2021-09-24 23:06:04 +0200

    intro to user create

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,34 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/babadee08/bookstore_users-api/domain/users"
+	"github.com/babadee08/bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	var user users.User
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		// TODO: Handle error
+		return
+	}
+
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println(err.Error())
+		// TODO: Handle JSON error
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		// TODO: Handle user creation error
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
