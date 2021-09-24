b6d0c40c34b0689f614c226d5c4be3c0e345b61a
damilare <damilare.durojaye@gmail.com>
2021-09-25 00:56:51 +0200

    Get User InMemory

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/babadee08/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
@@ -36,5 +37,17 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
--- services/users_service.go
+++ services/users_service.go
@@ -5,6 +5,14 @@ import (
 	"github.com/babadee08/bookstore_users-api/utils/errors"
 )
 
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	// if err := users.Validate(&user); err != nil {
 	if err := user.Validate(); err != nil {
