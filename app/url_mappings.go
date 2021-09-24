dd870a1df7b7a2c5e92029d9965a3ed2d5b1cb0e
damilare <damilare.durojaye@gmail.com>
2021-09-24 03:44:59 +0200

    Simple refactor, group controllers into individual packages

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,11 +1,13 @@
 package app
 
-import "github.com/babadee08/bookstore_users-api/controllers"
+import (
+	"github.com/babadee08/bookstore_users-api/controllers/ping"
+	"github.com/babadee08/bookstore_users-api/controllers/users"
+)
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
 
-	router.POST("/user/:user_id", controllers.GetUser)
-	router.POST("/user/search", controllers.SearchUser)
-	router.POST("/user", controllers.CreateUser)
+	router.GET("/user/:user_id", users.GetUser)
+	router.POST("/user", users.CreateUser)
 }
